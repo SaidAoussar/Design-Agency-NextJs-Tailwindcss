2245e626cd185dc5b7c8cb7d9db3160dafdeb395
SaidAoussar <aoussarsaid33@gmail.com>
2023-03-11 14:03:08 +0100

    feat: add services section

---
+++ components/services/services.tsx
@@ -0,0 +1,72 @@
+import Image from "next/image";
+import activityIcon from "@/public/img/services/activity.svg";
+import heartIcon from "@/public/img/services/heart.svg";
+import workIcon from "@/public/img/services/work.svg";
+
+import rightArrowIcon from "@/public/img/services/right-arrow.svg";
+import Container from "../container/container";
+
+const Services = () => {
+  return (
+    <section className="py-20">
+      <Container>
+        <h1 className="text-[#20B15A] mb-3 uppercase">What we do</h1>
+        <p className="text-3xl font-semibold mb-7 w-[495px]">
+          We provide the Perfect Solution to your business growth
+        </p>
+        <div className="services-wrapper flex justify-between">
+          <article className="py-10 px-7">
+            <div className="bg-[#D7F5DC] inline-block p-6 mb-3 rounded-2xl">
+              <Image
+                src={activityIcon}
+                alt="activity icon"
+                height="30"
+                width="30"
+              />
+            </div>
+            <h2 className="text-xl font-semibold mb-5">Grow Your Business</h2>
+            <p className="text-sm mb-8">
+              We help identify the best ways to improve your business
+            </p>
+            <button className="flex items-center gap-2 px-3 py-2">
+              Learn More
+              <Image src={rightArrowIcon} alt="right arrow icon" />
+            </button>
+          </article>
+          <article className="py-10 px-7">
+            <div className="bg-[#D7F5DC] inline-block p-6 mb-3 rounded-2xl">
+              <Image src={heartIcon} alt="heart icon" height="30" width="30" />
+            </div>
+            <h2 className="text-xl font-semibold mb-5">
+              Improve brand loyalty
+            </h2>
+            <p className="text-sm mb-8">
+              We help identify the best ways to improve your business
+            </p>
+            <button className="flex items-center gap-2 px-3 py-2">
+              Learn More
+              <Image src={rightArrowIcon} alt="right arrow icon" />
+            </button>
+          </article>
+          <article className="py-10 px-7">
+            <div className="bg-[#D7F5DC] inline-block p-6 mb-3 rounded-2xl">
+              <Image src={workIcon} alt="work icon" height="30" width="30" />
+            </div>
+            <h2 className="text-xl font-semibold mb-5">
+              Improve Business Model
+            </h2>
+            <p className="text-sm mb-8">
+              We help identify the best ways to improve your business
+            </p>
+            <button className="flex items-center gap-2 px-3 py-2">
+              Learn More
+              <Image src={rightArrowIcon} alt="right arrow icon" />
+            </button>
+          </article>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default Services;
