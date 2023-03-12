3971dcf81347168fea4cd49ca8cd751dbaf2ef97
SaidAoussar <aoussarsaid33@gmail.com>
2023-03-12 09:00:52 +0100

    feat: make intro section responsive

--- components/intro/intro.tsx
+++ components/intro/intro.tsx
@@ -3,23 +3,27 @@ import Image from "next/image";
 import Container from "../container/container";
 const Intro = () => {
   return (
-    <section>
+    <section className="mt-[64px] pt-10 mb-8 md:mb-0">
       <Container>
-        <div className="grid grid-cols-2 justify-center">
-          <div className="intro-text self-center">
-            <h1 className="text-5xl font-extrabold leading-[55px] mb-3">
+        <div className="lg:grid lg:grid-cols-2 lg:justify-center">
+          <div className="intro-text text-center md:mb-4 lg:text-left lg:mb-0 lg:self-center">
+            <h1 className="text-3xl font-bold mb-3 lg:text-5xl lg:font-extrabold lg:leading-[55px] lg:mb-3">
               Increase Your Customers Loyalty and Satisfaction
             </h1>
-            <p className="text-1xl font-medium mb-8">
+            <p className="mb-6 lg:text-xl lg:font-medium lg:mb-8">
               We help businesses like yours earn more customers, standout from
               competitors, make more money
             </p>
-            <button className="font-bold bg-[#20B15A] text-white py-4 px-7 rounded-2xl">
+            <button className="bg-[#20B15A] text-white font-medium py-3 px-5 rounded-xl lg:font-bold lg:py-4 lg:px-7 lg:rounded-2xl">
               Get Started
             </button>
           </div>
-          <div className="intro-img">
-            <Image src={ladyIntroImg} alt="lady intro image" />
+          <div className="intro-img hidden md:text-center md:block">
+            <Image
+              className="md:inline-block md:max-w-[68%] md:ml-32 lg:block lg:max-w-full lg:ml-0"
+              src={ladyIntroImg}
+              alt="lady intro image"
+            />
           </div>
         </div>
       </Container>
--- components/partners/partners.tsx
+++ components/partners/partners.tsx
@@ -1,20 +1,49 @@
+"use client";
+import Image from "next/image";
 import googleIcon from "@/public/img/partners/google-icon.svg";
 import trelloIcon from "@/public/img/partners/trello-icon.svg";
 import notionIcon from "@/public/img/partners/notion-icon.svg";
 import mondayIcon from "@/public/img/partners/monday-icon.svg";
 import slackIcon from "@/public/img/partners/slack-icon.svg";
 
-import Image from "next/image";
-
 const Partners = () => {
   return (
-    <div className="bg-[#F8FFF9] flex justify-evenly py-8">
-      <Image src={googleIcon} alt="google icon" />
-      <Image src={trelloIcon} alt="trello icon" />
-      <Image src={mondayIcon} alt="monday icon" />
-      <Image src={notionIcon} alt="notion icon" />
-      <Image src={slackIcon} alt="slack icon" />
-    </div>
+    <>
+      <div className="slider">
+        <div className="slide-track">
+          <div className="slide">
+            <Image src={googleIcon} alt="google icon" />
+          </div>
+          <div className="slide">
+            <Image src={trelloIcon} alt="trello icon" />
+          </div>
+          <div className="slide">
+            <Image src={mondayIcon} alt="monday icon" />
+          </div>
+          <div className="slide">
+            <Image src={notionIcon} alt="notion icon" />
+          </div>
+          <div className="slide">
+            <Image src={slackIcon} alt="slack icon" />
+          </div>
+          <div className="slide">
+            <Image src={googleIcon} alt="google icon" />
+          </div>
+          <div className="slide">
+            <Image src={trelloIcon} alt="trello icon" />
+          </div>
+          <div className="slide">
+            <Image src={mondayIcon} alt="monday icon" />
+          </div>
+          <div className="slide">
+            <Image src={notionIcon} alt="notion icon" />
+          </div>
+          <div className="slide">
+            <Image src={slackIcon} alt="slack icon" />
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
